1b05a8743ab84cc728d1f267a4817b6dd32ba694
Derick Beckwith <derick.beckwith@gmail.com>
2014-04-17 16:03:38 -0400

    Pass only the syscall number to kernel

--- testsyscall.c
+++ testsyscall.c
@@ -1,14 +1,15 @@
-#include <unistd.h>
 #include <sys/syscall.h>
 #include <stdio.h>
 
 int main(int argc, char *argv[])
 {
-	int retval = syscall(353, 24);
+	int retval = syscall(353);
+
 	if (retval < 0)
 	{
 		perror("COMP7506 system call returned with an error code.");
 	}
+
 	printf("COMP7506 system call returned %d.\n", retval);
 	return 0;
 }
